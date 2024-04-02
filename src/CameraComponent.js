5f32c3b1c8d485a2bfb39c44cc4992ea3715b97c
Sarah Erickson <98337750+sarah-erickson@users.noreply.github.com>
2024-04-02 15:52:32 -0400

    Update CameraComponent.js

--- src/CameraComponent.js
+++ src/CameraComponent.js
@@ -1,28 +1,35 @@
 import React, { useState, useCallback, useRef, useEffect } from 'react';
 import { Button } from '@mui/material';
 import Webcam from 'react-webcam';
+
 import Axios from 'axios';
+
+
 
 function CameraComponent() {
   const webcamRef = useRef(null);
   const [capturedImage, setCapturedImage] = useState(null);
   const [imageFile, setImageFile] = useState(null);
   const [fileName, setFileName] = useState('');
-  const [loading, setLoading] = useState(false); // New state for loading
+  const [loading, setLoading] = useState(false);
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setCapturedImage(imageSrc);
     let blobFile = base64toImage(imageSrc);
+    //webcamRef
+    //let blobFileName = ;
     let file = new File([blobFile], 'screenCapture.jpeg');
     setImageFile(file);
-    setFileName('screenCapture.jpeg');
+    setFileName('screenCapture.jpeg')
   }, [webcamRef]);
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
+    
     setImageFile(file);
     setFileName(file.name);
+    console.log(file.name);
     const reader = new FileReader();
     reader.onloadend = () => {
       setCapturedImage(reader.result);
@@ -32,7 +39,8 @@ function CameraComponent() {
     }
   };
 
-  const base64toImage = function (image) {
+
+  const base64toImage = function (image){
     const byteString = atob(image.split(',')[1]);
     const ab = new ArrayBuffer(byteString.length);
     const ia = new Uint8Array(ab);
@@ -45,11 +53,13 @@ function CameraComponent() {
     return newBlob;
   };
 
-  const submitImage = () => {
-    setLoading(true); // Set loading state to true when submitting image
+
+  const submitImage = () =>{
+    setLoading(true);
     const formData = new FormData();
     var im = imageFile;
     formData.append('file', im, fileName);
+    console.log(fileName);
     
     const url = "http://localhost:5000/segment";
 
@@ -60,21 +70,22 @@ function CameraComponent() {
     })
     .then(response => {
       console.log(response.data);
-      setLoading(false); // Set loading state to false when response is received
+      setLoading(false);
     })
     .catch(error => {
       console.log(error);
-      setLoading(false); // Set loading state to false on error
+      setLoading(false);
     });
   };
-
   const clearImage = () => {
     setCapturedImage(null);
     setImageFile(null);
     setFileName('');
   };
 
+
   useEffect(() => {
+    // Scroll to the photo when captured or uploaded
     const photoSection = document.getElementById('photo-section');
     if (photoSection) {
       photoSection.scrollIntoView({ behavior: 'smooth' });
@@ -83,80 +94,81 @@ function CameraComponent() {
 
   return (
     <div style={{ textAlign: 'center', backgroundColor: '#000', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', height: '200vh', overflow: 'auto' }}>
-      <h1 style={{ color: '#fff', marginBottom: '20px', fontFamily: 'Arial, sans-serif', fontSize: '2rem', display: 'block', margin: '0 auto', zIndex: '2', marginTop: '10px' }}>Clotoure Demo</h1>
-      <div style={{ margin: '0 auto', position: 'relative', width: '400px', height: '300px', zIndex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginBottom: '20px' }}>
-        <Webcam
-          audio={false}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          className="webcam"
-          mirrored={true}
-          style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
-        />
-        <div style={{ marginTop: 'calc(300px + 1in)', position: 'relative', zIndex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <button style={{ marginRight: '10px', padding: '10px 20px', fontSize: '1rem', backgroundColor: '#000', color: '#fff', borderRadius: '5px', cursor: 'pointer', transition: 'background-color 0.3s ease', zIndex: '1' }} onClick={capture}>Capture</button>
-          <label htmlFor="file-upload" className="custom-file-upload" style={{ padding: '10px 20px', fontSize: '1rem', color: '#fff', borderRadius: '5px', cursor: 'pointer', transition: 'background-color 0.3s ease', zIndex: '1', border: 'none', backgroundColor: 'transparent', position: 'relative' }}>
-            Choose file
-            <input id="file-upload" type="file" accept="image/*" onChange={handleFileUpload} style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', opacity: '0', cursor: 'pointer' }} />
-          </label>
-        </div>
+    <h1 style={{ color: '#fff', marginBottom: '20px', fontFamily: 'Arial, sans-serif', fontSize: '2rem', display: 'block', margin: '0 auto', zIndex: '2', marginTop: '10px' }}>Clotoure Demo</h1>
+    <div style={{ margin: '0 auto', position: 'relative', width: '400px', height: '300px', zIndex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginBottom: '20px' }}>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        className="webcam"
+        mirrored={true}
+        style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
+      />
+      <div style={{ marginTop: 'calc(300px + 1in)', position: 'relative', zIndex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <button style={{ marginRight: '10px', padding: '10px 20px', fontSize: '1rem', backgroundColor: '#000', color: '#fff', borderRadius: '5px', cursor: 'pointer', transition: 'background-color 0.3s ease', zIndex: '1' }} onClick={capture}>Capture</button>
+        <label htmlFor="file-upload" className="custom-file-upload" style={{ padding: '10px 20px', fontSize: '1rem', color: '#fff', borderRadius: '5px', cursor: 'pointer', transition: 'background-color 0.3s ease', zIndex: '1', border: 'none', backgroundColor: 'transparent', position: 'relative' }}>
+          Choose file
+          <input id="file-upload" type="file" accept="image/*" onChange={handleFileUpload} style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', opacity: '0', cursor: 'pointer' }} />
+        </label>
       </div>
-      {capturedImage && (
-        <div id="photo-section" style={{ marginTop: '200px', display: 'flex', flexDirection: 'column', alignItems: 'center', zIndex: '1' }}>
-          <h2 style={{ color: '#fff', marginBottom: '10px' }}>YOUR PHOTO!</h2>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <img
-              src={capturedImage}
-              alt="Captured"
+    </div>
+    {capturedImage && (
+      <div id="photo-section" style={{ marginTop: '200px', display: 'flex', flexDirection: 'column', alignItems: 'center', zIndex: '1' }}>
+        <h2 style={{ color: '#fff', marginBottom: '10px' }}>YOUR PHOTO!</h2>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <img
+            src={capturedImage}
+            alt="Captured"
+            style={{
+              width: '300px',
+              height: 'auto',
+              border: '2px solid #ccc',
+              borderRadius: '10px',
+              margin: '0 auto',
+            }}
+          />
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <button
               style={{
-                width: '300px',
-                height: 'auto',
-                border: '2px solid #ccc',
-                borderRadius: '10px',
-                margin: '0 auto',
+                marginTop: '10px',
+                padding: '10px 20px',
+                fontSize: '1rem',
+                backgroundColor: '#000',
+                color: '#fff',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                transition: 'background-color 0.3s ease',
+                zIndex: '1',
               }}
-            />
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <button
-                style={{
-                  marginTop: '10px',
-                  padding: '10px 20px',
-                  fontSize: '1rem',
-                  backgroundColor: '#000',
-                  color: '#fff',
-                  borderRadius: '5px',
-                  cursor: 'pointer',
-                  transition: 'background-color 0.3s ease',
-                  zIndex: '1',
-                }}
-                onClick={submitImage}
-                disabled={loading} // Disable button when loading
-              >
-                {loading ? 'Loading...' : 'Segment'}
-              </button>
-              <button
-                style={{
-                  marginTop: '10px',
-                  marginLeft: '10px',
-                  padding: '10px 20px',
-                  fontSize: '1rem',
-                  backgroundColor: '#ccc',
-                  color: '#000',
-                  borderRadius: '5px',
-                  cursor: 'pointer',
-                  transition: 'background-color 0.3s ease',
-                  zIndex: '1',
-                }}
-                onClick={clearImage}
-              >
-                Clear
-              </button>
-            </div>
+              onClick={submitImage}
+              disabled={loading} // Disable button when loading
+            >
+              {loading ? 'Loading...' : 'Segment'}
+            </button>
+            <button
+              style={{
+                marginTop: '10px',
+                marginLeft: '10px',
+                padding: '10px 20px',
+                fontSize: '1rem',
+                backgroundColor: '#ccc',
+                color: '#000',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                transition: 'background-color 0.3s ease',
+                zIndex: '1',
+              }}
+              onClick={clearImage}
+            >
+              Clear
+            </button>
           </div>
         </div>
-      )}
-    </div>
-  );
+      </div>
+    )}
+  </div>
+);
 }
 
 export default CameraComponent;
+
